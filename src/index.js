e3b62a8c0dc32f136225f38cbba0f6e39b53bdf9
Arthur Vinicius <thuur.vss@gmail.com>
2022-06-11 21:44:39 -0300

    Final da implementação

--- src/index.js
+++ src/index.js
@@ -10,20 +10,83 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({
+      error: "User dont exists"
+    });
+  }
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ((user.pro) || (!user.pro && user.todos.length < 10)) {
+    request.user = user;
+    return next();
+  }
+  return response.status(403).json({
+    error: "User does not have premium account"
+  });
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const user = users.find((user) => user.username === username);
+  if (user) {
+    const todo = user.todos.find((todo) => todo.id === id);
+
+    // valida se id eh uuid
+    const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/
+    const idIsValid = regex.test(id)
+    if (!idIsValid) {
+      return response.status(400).json({
+        error: "Id is not a uuid"
+      });
+    }
+    // valida que id do todo eh do user informado
+    const itsFromUser = user.todos.some(todo => todo.id === id);
+    if (!itsFromUser) {
+      return response.status(404).json({
+        error: "Todo don't exists or don't come from this user"
+      });
+    }
+    if (idIsValid && itsFromUser) {
+      // manda ele pra frente no request
+      request.user = user;
+      request.todo = todo;
+      return next();
+    }
+  }
+  return response.status(404).json({
+    error: "Todo don't exists or don't come from this user"
+  });
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+  if (!user) {
+    return response.status(404).json({
+      error: "User dont exists"
+    });
+  }
+  request.user = user;
+  return next();
 }
+
+app.get('/', (request, response) => {
+  return response.json({
+    status: "on-line"
+  })
+})
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
